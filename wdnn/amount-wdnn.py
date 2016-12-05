4e1512af58c7a0512f3b32e1146312abc4d08416
YTorii <metalwarrior_type_jp@hotmail.com>
2016-12-05 10:32:18 +0000

    Resolved warning for unmatched rank in continuous input

--- wdnn/amount-wdnn.py
+++ wdnn/amount-wdnn.py
@@ -16,11 +16,10 @@ def make_columns_list(name_list, num_range):
   return columns_list
       
 # precipitation = 降水量
-CONTINUOUS_COLUMNS_NAME = [ "temp", "wind", "prec" ]
-CONTINUOUS_COLUMNS_NAME_REDUCED = [ "temp", "prec" ]
+#CONTINUOUS_COLUMNS_NAME = [ "temp", "wind", "prec" ]
+CONTINUOUS_COLUMNS_NAME = [ "temp", "prec" ]
 CONTINUOUS_COLUMNS_NUM = 10
 CONTINUOUS_COLUMNS = make_columns_list(CONTINUOUS_COLUMNS_NAME, CONTINUOUS_COLUMNS_NUM)
-CONTINUOUS_COLUMNS_REDUCED = make_columns_list(CONTINUOUS_COLUMNS_NAME_REDUCED, CONTINUOUS_COLUMNS_NUM)
 
 CATEGORICAL_COLUMNS = [ "month", "days" ]
 
@@ -48,27 +47,27 @@ def build_estimator():
   deep_columns = [ m_embed, d_embed ]
 
   # Inputs with real values(like 1.03, 2.3)
-  for col in CONTINUOUS_COLUMNS_REDUCED:
-    #deep_columns.append(tf.contrib.layers.real_valued_column(col,dimension=16))
-    deep_columns.append(tf.contrib.layers.real_valued_column(col))
+  for col in CONTINUOUS_COLUMNS:
+    deep_columns.append(tf.contrib.layers.real_valued_column(col,dimension=3))
+    #deep_columns.append(tf.contrib.layers.real_valued_column(col, dimension=2))
 
   # Relevance among inputs, this shuold be derived from catagolized values.
   for i in range(CONTINUOUS_COLUMNS_NUM):
     temp = 'temp%s' % (i)
-#    wind = 'wind%s' % (i)
+    #wind = 'wind%s' % (i)
     prec = 'prec%s' % (i)
 
     wide_columns.append(
       tf.contrib.layers.crossed_column([
         tf.contrib.layers.bucketized_column(tf.contrib.layers.real_valued_column(temp), boundaries=[0, 10, 20, 25, 35, 40]),
-#        #tf.contrib.layers.bucketized_column(tf.contrib.layers.real_valued_column(wind), boundaries=[0, 0.3, 0.5, 1.0, 2.0, 3.0]),
+        #tf.contrib.layers.bucketized_column(tf.contrib.layers.real_valued_column(wind), boundaries=[0, 0.3, 0.5, 1.0, 2.0, 3.0]),
         tf.contrib.layers.bucketized_column(tf.contrib.layers.real_valued_column(prec), boundaries=[0, 0.5, 1.0, 2.0, 3.0, 4.0])
       ], hash_bucket_size=int(1e6)))
 
   # Optimizers
   grad_opt = tf.train.GradientDescentOptimizer(learning_rate=0.01)
   adagrad_opt = tf.train.AdagradOptimizer(learning_rate=0.02)
-  adam_opt = tf.train.AdamOptimizer(learning_rate=0.05)
+  adam_opt = tf.train.AdamOptimizer()
   padagrad_opt = tf.train.ProximalAdagradOptimizer( learning_rate=0.1, l1_regularization_strength=0.001, l2_regularization_strength=0.001)
 
   #m = tf.contrib.learn.DNNRegressor(feature_columns=feature_columns, optimizer=adam_opt, dropout=0.15, hidden_units=[ 50, 20 ])
@@ -76,14 +75,14 @@ def build_estimator():
         linear_feature_columns=wide_columns,
         dnn_feature_columns=deep_columns,
         dnn_optimizer=adam_opt,
-        dnn_dropout=0.5,
-        dnn_hidden_units=[ 10, 5 ])
+        dnn_hidden_units=[ 30, 50, 1 ])
 
   return m
 
 def input_fn(df):
-  #print(pd.concat([df[l] for l in df.columns[0:16].values], axis=1))
-  continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
+  # Tensor rank should be 2 (e.g. [[1.], [2]]), so put shape as [size, 1]
+  df_size = df[k].shape[0]
+  continuous_cols = {k: tf.constant(df[k].values, shape=[df_size,1]) for k in CONTINUOUS_COLUMNS}
   categorical_cols = {k: tf.SparseTensor(
     indices=[[i,0] for i in range(df[k].size)],
     values=df[k].values.astype(str),
@@ -92,18 +91,22 @@ def input_fn(df):
 
   feature_cols = dict(continuous_cols)
   feature_cols.update(categorical_cols)
-  label = tf.constant(df[LABEL_COLUMN].values)
+
+  # dtype should be epilicitly declarated as float when all labels are inetger
+  label = tf.constant(df[LABEL_COLUMN].values, dtype=tf.float64)
 
   return feature_cols, label
 
 def train_and_eval():
   df_train = pd.read_csv(tf.gfile.Open(TRAIN_FILENAME),
     names=INPUT_COLUMNS,
+    #dtype=tf.float64
     skipinitialspace=True,
     engine="python")
 
   df_test = pd.read_csv(tf.gfile.Open(TEST_FILENAME),
     names=INPUT_COLUMNS,
+    #dtype=tf.float64,
     skipinitialspace=True,
     engine="python")
 
@@ -111,7 +114,7 @@ def train_and_eval():
   df_test = df_test.dropna(how='any', axis=0)
 
   model = build_estimator()
-  model.fit(input_fn=lambda: input_fn(df_train), steps=100000)
+  model.fit(input_fn=lambda: input_fn(df_train), steps=10000)
   test_results = model.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
   for key in sorted(test_results):
     print("%s: %s" % (key, test_results[key]))
